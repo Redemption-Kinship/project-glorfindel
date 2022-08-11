0c0db3ba404cfc8a44e4b19aae289b0176af4f5f
Vuldyn <vuldynironheart@gmail.com>
2022-08-11 16:05:33 +1000

    discord bot initial setup
    Dependencies and basic bot startup have been added.

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // This is the entry point for our app, and should be the only root-level file in the directory.
 
-console.log('Hello World!');
+// Require necessary discord.js classes
+const {Client, GatewayIntentBits} = require('discord.js');
+// Import our client token
+const {token} = require('./discord_util/config.json');
+
+// Create a new client instance. The GatewayIntentBits is required.
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+// When the client logs in successfully, this code will run once only:
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+// Login to discord using the client token. This should always be at the bottom.
+client.login(token);
